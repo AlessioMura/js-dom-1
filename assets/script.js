be6790e3e6ead1265680c92da261d9fdaf52e854
AlessioMura <alessiomura03@gmail.com>
2024-10-08 17:30:47 +0200

    added 'turnOn' func

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,13 @@ const light_off_el = document.getElementById('off')
 const light_on_el = document.getElementById('on')
 
 //processing
+btn_el.addEventListener('click', () => {
+
+    if (btn_el.innerHTML.includes('TURN ON')) {
+        
+        light_off_el.classList.add('d-none');
+        light_on_el.classList.remove('d-none');
+
+    }
+
+});
